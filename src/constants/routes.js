5a158b460c6b1c9fb035fbf45fab651938710388
Jejo <jejomardorongon@gmail.com>
2020-08-27 20:50:14 +0800

    removed some menus

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -9,14 +9,4 @@ export default [
     path: "/page-2",
     text: "Menu 2",
   },
-  {
-    id: 3,
-    path: "/page-3",
-    text: "Menu 3",
-  },
-  {
-    id: 4,
-    path: "/page-4",
-    text: "Menu 4",
-  },
 ]
